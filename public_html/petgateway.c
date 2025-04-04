6ef657c8ae74e67fe5747ad43da8f981cfd28ae2
petbrain <uebi@tutanota.com>
2025-04-04 04:32:45 +0000

    Simplified code.

--- public_html/petgateway.c
+++ public_html/petgateway.c
@@ -30,28 +30,18 @@ void print_error(char* fmt, ...)
         fputs("Out of memory", stdout);
     } else {
         // escape double quotes and newlines for JSON output
-        unsigned escaped_msg_len = msg_len;
         for(int i = 0; i < msg_len; i++) {
             char c = msg[i];
-            if (c == '"' || c == '\n') {
-                escaped_msg_len++;
-            }
-        }
-        char escaped_msg[escaped_msg_len + 1];
-        for(int i = 0, o = 0; i < msg_len; i++) {
-            char c = msg[i];
             if (c == '"') {
-                escaped_msg[o++] = '\\';
-                escaped_msg[o++] = c;
+                putchar('\\');
+                putchar(c);
             } else if (c == '\n') {
-                escaped_msg[o++] = '\\';
-                escaped_msg[o++] = 'n';
+                putchar('\\');
+                putchar('n');
             } else {
-                escaped_msg[o++] = c;
+                putchar(c);
             }
         }
-        escaped_msg[escaped_msg_len] = 0;
-        fputs(escaped_msg, stdout);
         free(msg);
     }
     fputs(error_end, stdout);
@@ -82,21 +72,21 @@ int main(int argc, char* argv[])
         if (var == nullptr) {
             break;
         }
-        char* delimiter = strchr(var, '=');
-        char* value;
-        unsigned key_len = 0;
-        if (delimiter == nullptr) {
-            value = "";
-        } else {
-            key_len = delimiter - var;
-            value = delimiter + 1;
+        fputs("        ", log);  // indent
+        // print NAME=VALUE as NAME: VALUE
+        for (;;) {
+            char c = *var++;
+            if (c == 0) {
+                break;
+            }
+            if (c == '=') {
+                fputc(':', log);
+                fputc(' ', log);
+            } else {
+                fputc(c, log);
+            }
         }
-        char key[key_len + 1];
-        if (key_len) {
-            strncpy(key, var, key_len);
-        }
-        key[key_len] = 0;
-        fprintf(log, "        %s: %s\n", key, value);
+        fputc('\n', log);
     }
     fclose(log);
 
